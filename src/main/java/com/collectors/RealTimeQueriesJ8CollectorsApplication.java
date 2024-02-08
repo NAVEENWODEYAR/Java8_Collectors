1b9a0847b74d1c4ea64c21856f605d8aa4bffbab
Naveen <naveenkrishnawodeyar@gmail.com>
2024-02-08 08:12:53 +0530

    initial commit

---
+++ src/main/java/com/collectors/RealTimeQueriesJ8CollectorsApplication.java
@@ -0,0 +1,13 @@
+package com.collectors;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealTimeQueriesJ8CollectorsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RealTimeQueriesJ8CollectorsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/collectors/RealTimeQueriesJ8CollectorsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.collectors;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealTimeQueriesJ8CollectorsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
