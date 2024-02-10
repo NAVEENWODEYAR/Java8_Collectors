7426ae6d009a3adc27a33ecc8e141da918a152ef
Naveen <naveenkrishnawodeyar@gmail.com>
2024-02-10 19:21:22 +0530

    solve using Collections

--- src/main/java/com/collectors/codechallenge/StringOccurances.java
+++ src/main/java/com/collectors/codechallenge/StringOccurances.java
@@ -1,6 +1,8 @@
 
 package com.collectors.codechallenge;
 
+import java.util.Arrays;
+
 /**
  *@author Naveen K Wodeyar,
  *@date 10-02-2024 
@@ -9,16 +11,19 @@ public class StringOccurances {
     /**
      * find the number of occurrences of an word in an String,
      */
-    public void stringOccurrence(String st) {
-	String str[] = st.toLowerCase().split("");
+    static void stringOccurrence(String st,String s) {
+	String str[] = st.toLowerCase().split(" ");
 	int count=0;
-	for(int i=0; i<=str.length; i++) {
+	System.out.println(Arrays.toString(str));
+	for(int i=0; i<str.length; i++) {
 	    if(str[i].equalsIgnoreCase(st))
-		count++;
+		count = count+1;
+	    else
+		count = 1;
 	}
 	System.out.println("String "+st+" occurs,"+count+" times");
     }
     public static void main(String[] args) {
-
+	stringOccurrence("Welcome to Java programming,Java is secure & strong","Java");
     }
 }
