6eee09d6c8c3884d02bd5104159d03ba8160d309
Naveen <naveenkrishnawodeyar@gmail.com>
2024-02-13 22:46:01 +0530

    Creating specific collection: toCollection()

--- src/main/java/com/collectors/methods/CollectorsMethods.java
+++ src/main/java/com/collectors/methods/CollectorsMethods.java
@@ -1,6 +1,7 @@
 package com.collectors.methods;
 
 import java.util.Arrays;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -12,9 +13,13 @@ public class CollectorsMethods {
 
     public static void main(String[] args) {
 	
-	//1. Creating list: toList()
+	//1. Creating list: toList(),
 	List<Integer> numList = Arrays.asList(1,2,3,4,5,6,7,8,9);
 			numList.parallelStream().map(n->n+1).collect(Collectors.toList()).forEach(System.out::print);
+    
+	//2. Creating specific collection: toCollection(),
+	LinkedList<Integer> collect = numList.stream().map(n->n*n).collect(Collectors.toCollection(LinkedList::new));
+	System.out.println(collect);
     }
 
 }
