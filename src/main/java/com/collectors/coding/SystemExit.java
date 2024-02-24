c2f192ac2c526f1b0176cf0ce6c5b8fb26029426
Naveen <naveenkrishnawodeyar@gmail.com>
2024-02-24 23:10:00 +0530

    JVM will terminate if it encounters System.exit(0) in try block

--- src/main/java/com/collectors/coding/SystemExit.java
+++ src/main/java/com/collectors/coding/SystemExit.java
@@ -7,8 +7,10 @@ public class SystemExit {
 	int a=0;int b=0;
 	try {
 	    System.out.println(a+b);
+	    System.exit(0);
 	} catch (Exception e) {
 	    System.out.println(e.getStackTrace());
+	    // JVM will terminate here, finally block won't execute,
 	    System.exit(0);
 	}finally {
 	    System.out.println("Finally block,");
