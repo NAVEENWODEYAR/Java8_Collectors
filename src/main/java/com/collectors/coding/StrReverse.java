8c85c74884351a75670d23c1d43e38ec3c520415
Naveen <naveenkrishnawodeyar@gmail.com>
2024-02-24 23:31:40 +0530

    String reverse using charAt

---
+++ src/main/java/com/collectors/coding/StrReverse.java
@@ -0,0 +1,21 @@
+package com.collectors.coding;
+
+public class StrReverse {
+
+    /**
+     * @author Naveen K Wodeyar
+     * Write a Java program to reverse a given string without using any built-in functions,
+     */
+    
+    static String stringReverse(String st) {
+	String rev="";
+	for(int i=st.length()-1; i>=0; i--) {
+	    rev += st.charAt(i);
+	}
+	return rev;
+    }
+    public static void main(String[] args) {
+	System.out.println("String Reverse");
+	System.out.println(stringReverse("System"));
+    }
+}
