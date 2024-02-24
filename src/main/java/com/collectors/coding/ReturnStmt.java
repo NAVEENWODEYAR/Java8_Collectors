4de33eb2c24d55a4af0738f372e461b19abca402
Naveen <naveenkrishnawodeyar@gmail.com>
2024-02-24 23:07:30 +0530

    JVM will terminate here, finally block won't execute

---
+++ src/main/java/com/collectors/coding/ReturnStmt.java
@@ -0,0 +1,23 @@
+package com.collectors.coding;
+
+public class ReturnStmt {
+
+    static int add(int a,int b) {
+	System.out.println("return in try and catch block,");
+	try {
+	    System.out.println("inside try block");
+	    return a-b;
+	} catch (Exception e) {
+	    System.out.println(e.getMessage());
+	    return -1;
+	}
+	finally {
+	    System.out.println("inside finally blcok,");
+	}
+    }
+    public static void main(String[] args) {
+	add(1, 1);
+
+    }
+
+}
---
+++ src/main/java/com/collectors/coding/SystemExit.java
@@ -0,0 +1,18 @@
+package com.collectors.coding;
+
+public class SystemExit {
+
+    public static void main(String[] args) {
+	System.out.println("System.exit()");
+	int a=0;int b=0;
+	try {
+	    System.out.println(a+b);
+	} catch (Exception e) {
+	    System.out.println(e.getStackTrace());
+	    System.exit(0);
+	}finally {
+	    System.out.println("Finally block,");
+	}
+    }
+
+}
