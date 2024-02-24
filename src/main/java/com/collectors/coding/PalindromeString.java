4fe9804cbb339ad4013e625ebc514fdca6823467
Naveen <naveenkrishnawodeyar@gmail.com>
2024-02-24 23:40:52 +0530

    Exception in thread main java.lang.StringIndexOutOfBoundsException: Index 5 out of bounds for length 5

---
+++ src/main/java/com/collectors/coding/PalindromeString.java
@@ -0,0 +1,34 @@
+
+package com.collectors.coding;
+
+public class PalindromeString {
+
+    private static boolean palindrome;
+
+    /**
+     * Palindrome using length,
+     */
+    static boolean isPalindrome(String st) {
+	st.toLowerCase();
+	int left = 0;
+	int right = st.length()-1;
+	while(left<right) {
+	    if(st.charAt(left)!=st.charAt(right)) {
+		return false;
+	    }
+	    left++;
+	    right++;
+	}
+	return true;
+    }
+    
+    public static void main(String[] args) {
+	System.out.println("Palindrome String");
+	palindrome = isPalindrome("level");
+	if(palindrome)
+	    System.out.println("Palindrome");
+	else
+	    System.out.println("Not an Palindrome");
+    }
+
+}
