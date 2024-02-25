ad74a754edee3ce465147bdc56b607c80d5a2eb3
Naveen <naveenkrishnawodeyar@gmail.com>
2024-02-26 00:15:50 +0530

    logOut

--- src/main/java/com/collectors/coding/PalindromeString.java
+++ src/main/java/com/collectors/coding/PalindromeString.java
@@ -24,6 +24,7 @@ public class PalindromeString {
     
     public static void main(String[] args) {
 	Scanner sc = new Scanner(System.in);
+	System.out.println("Enter an string:");
 	String st = sc.next();
 	if(isPalindrome(st))
 	    System.out.println("Palindrome");
