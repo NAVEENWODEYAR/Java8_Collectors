1806c301ec14474e54baa92f71944f7283da393b
Naveen <naveenkrishnawodeyar@gmail.com>
2024-03-03 10:51:28 +0530

    String frequency

---
+++ src/main/java/com/collectors/coding/StringFrequency.java
@@ -0,0 +1,22 @@
+package com.collectors.coding;
+
+public class StringFrequency {
+
+    /**
+     * @author Naveen K Wodeyar
+     * @date 25-02-2024,
+     * Write a Java program to reverse a given string without using any built-in functions,
+     */
+    
+    static String stringReverse(String st) {
+	String rev="";
+	for(int i=st.length()-1; i>=0; i--) {
+	    rev += st.charAt(i);
+	}
+	return rev;
+    }
+    public static void main(String[] args) {
+	System.out.println("\nString Reverse");
+	System.out.println(stringReverse("System"));
+    }
+}
