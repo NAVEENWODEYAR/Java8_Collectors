ccb473038c497bff2c54ac172328b98c75321dfb
Naveen <naveenkrishnawodeyar@gmail.com>
2024-03-11 22:15:43 +0530

    sit env deleted

--- src/main/java/com/collectors/coding/PalindromeString.java
+++ src/main/java/com/collectors/coding/PalindromeString.java
@@ -21,7 +21,7 @@ public class PalindromeString {
     
     public static void main(String[] args) {
 	Scanner sc = new Scanner(System.in);
-	System.out.println("Enter an string:");
+	System.out.println("\nEnter an string:");
 	String st = sc.next();
 	if(isPalindrome(st))
 	    System.out.println("Palindrome");
