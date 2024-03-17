4fd456805836b7067db6053afecaa3bb5142ce13
Naveen <naveenkrishnawodeyar@gmail.com>
2024-03-17 22:27:56 +0530

    logOut

--- src/main/java/com/collectors/coding/ExceptionOverRiding.java
+++ src/main/java/com/collectors/coding/ExceptionOverRiding.java
@@ -3,14 +3,14 @@ package com.collectors.coding;
 // Parent class with NullPointerException,
 class Parent{
 	public void test() throws NullPointerException{
-	    System.out.println("Parent Class");
+	    System.out.println("Parent Class test()");
 	    throw new NullPointerException("NullPointerException in Parent class,");
 	}
 }
 class Child extends Parent{
     @Override
     public void test() throws RuntimeException{
-	System.out.println("Child class");
+	System.out.println("Child class test()");
 	throw new RuntimeException("RuntimeException,");
     }
 }
