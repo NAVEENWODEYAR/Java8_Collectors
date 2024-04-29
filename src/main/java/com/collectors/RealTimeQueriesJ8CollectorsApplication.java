2a219f6b3f2133c1b3f36f0e93f7a8d8b5327eb8
Naveen <naveenkrishnawodeyar@gmail.com>
2024-04-29 22:57:00 +0530

    swagger config errors

--- src/main/java/com/collectors/RealTimeQueriesJ8CollectorsApplication.java
+++ src/main/java/com/collectors/RealTimeQueriesJ8CollectorsApplication.java
@@ -12,6 +12,5 @@ public class RealTimeQueriesJ8CollectorsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RealTimeQueriesJ8CollectorsApplication.class, args);
-		System.out.println(Thread.currentThread().getName());
 	}
 }
