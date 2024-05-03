710958c2b2d23dbee00b6221fb333d26f44cf15f
Naveen <naveenkrishnawodeyar@gmail.com>
2024-05-03 07:54:33 +0530

    swagger annotations removed

--- src/main/java/com/collectors/RealTimeQueriesJ8CollectorsApplication.java
+++ src/main/java/com/collectors/RealTimeQueriesJ8CollectorsApplication.java
@@ -3,11 +3,7 @@ package com.collectors;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-import io.swagger.v3.oas.annotations.info.Info;
-
 @SpringBootApplication
-@OpenAPIDefinition(info = @Info(title = "DEMO COLLECTORS",version = "1.0",description = "Demo Collectors Methods"))
 public class RealTimeQueriesJ8CollectorsApplication {
 
 	public static void main(String[] args) {
