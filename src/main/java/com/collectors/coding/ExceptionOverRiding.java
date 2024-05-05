888170a75374f232aaaeba2ee467b967b1bcbcc5
Naveen <naveenkrishnawodeyar@gmail.com>
2024-05-05 21:12:30 +0530

    recursive call

--- src/main/java/com/collectors/coding/ExceptionOverRiding.java
+++ src/main/java/com/collectors/coding/ExceptionOverRiding.java
@@ -4,6 +4,7 @@ package com.collectors.coding;
 class Parent{
 	public void test() throws NullPointerException{
 	    System.out.println("Parent Class");
+	    test();
 	    throw new NullPointerException("NullPointerException in Parent class,");
 	}
 }
@@ -12,6 +13,7 @@ class Child extends Parent{
     @Override
     public void test() throws RuntimeException{
 	System.out.println("Child class");
+	test();
 	throw new RuntimeException("RuntimeException in Child class,");
     }
 }
