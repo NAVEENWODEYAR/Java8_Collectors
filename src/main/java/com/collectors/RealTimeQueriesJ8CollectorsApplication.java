e4072309d949527c574849aae74cbaaf55d4f9ed
Naveen <naveenkrishnawodeyar@gmail.com>
2024-05-08 09:15:26 +0530

    swagger added

--- src/main/java/com/collectors/RealTimeQueriesJ8CollectorsApplication.java
+++ src/main/java/com/collectors/RealTimeQueriesJ8CollectorsApplication.java
@@ -3,10 +3,16 @@ package com.collectors;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import lombok.extern.slf4j.Slf4j;
+
 @SpringBootApplication
+@OpenAPIDefinition
+@Slf4j
 public class RealTimeQueriesJ8CollectorsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RealTimeQueriesJ8CollectorsApplication.class, args);
+		log.error("@OpenAPIDefination");
 	}
 }
