143c567cf54cc3324d1e6104e31b35149e5abfc9
Naveen <naveenkrishnawodeyar@gmail.com>
2024-05-17 23:25:36 +0530

    string frequency

---
+++ src/main/java/com/collectors/coding/StringFrequency2.java
@@ -0,0 +1,21 @@
+package com.collectors.coding;
+
+import java.util.*;
+
+
+public class StringFrequency2 {
+    /**
+     * @author Naveen K Wodeyar
+     * @date 25-02-2024,
+     */
+   static int stringFrequency(String st, String s) {
+       List<String> wordsList = Arrays.asList(st.toLowerCase().split(" "));
+       return Collections.frequency(wordsList, s);
+   }
+   
+    public static void main(String[] args) {
+	System.out.println("\nString Frequency");
+	int stringFrequency = stringFrequency("level","l");
+	System.out.println(stringFrequency);
+    }
+}
