a99bb4c60bbda9d61c1518b8f64253e27adb82ae
Naveen <naveenkrishnawodeyar@gmail.com>
2024-05-17 23:48:43 +0530

    frequency using map

--- src/main/java/com/collectors/coding/StringFrequency2.java
+++ src/main/java/com/collectors/coding/StringFrequency2.java
@@ -1,21 +1,22 @@
 package com.collectors.coding;
 
 import java.util.*;
-
+import java.util.function.Function;
+import java.util.stream.*;
 
 public class StringFrequency2 {
-    /**
-     * @author Naveen K Wodeyar
-     * @date 25-02-2024,
-     */
-   static int stringFrequency(String st, String s) {
-       List<String> wordsList = Arrays.asList(st.toLowerCase().split(" "));
-       return Collections.frequency(wordsList, s);
-   }
+
+	 public static <Character> Map<Character, Long>  frequencyMap(Stream<Character> elements) {
+	        return elements.collect(
+	            Collectors.groupingBy(
+	            Function.identity(),
+	            HashMap::new, // can be skipped
+	            Collectors.counting()
+	            )
+	        );
+	    }
    
     public static void main(String[] args) {
-	System.out.println("\nString Frequency");
-	int stringFrequency = stringFrequency("level","l");
-	System.out.println(stringFrequency);
+
     }
 }
