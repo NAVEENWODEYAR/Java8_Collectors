5e2f9ac545c40cf251c13a984bc95bad07ce9e8e
Naveen <naveenkrishnawodeyar@gmail.com>
2024-05-31 23:58:47 +0530

    Most repeated element in array

---
+++ src/main/java/com/collectors/arrList/MostRepeatedElement.java
@@ -0,0 +1,19 @@
+
+package com.collectors.arrList;
+
+/**
+ * @author Naveen K Wodeyar
+ * @apiNote,find the most repeated element in an array
+ *
+ */
+public class MostRepeatedElement {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
