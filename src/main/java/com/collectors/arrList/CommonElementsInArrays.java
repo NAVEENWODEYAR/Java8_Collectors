d0345ef4ca1932bd6849feb2fd84d7ee3574464d
Naveen <naveenkrishnawodeyar@gmail.com>
2024-05-31 23:19:51 +0530

    list.stream().filter(li)

---
+++ src/main/java/com/collectors/arrList/CommonElementsInArrays.java
@@ -0,0 +1,24 @@
+
+package com.collectors.arrList;
+
+import java.util.*;
+
+/**
+ * @author Naveen K Wodeyar
+ * @date 31/05/2024
+ *
+ */
+public class CommonElementsInArrays {
+	 static List<Integer> list1 = Arrays.asList(71, 21, 34, 89, 56, 28);
+     
+     static List<Integer> list2 = Arrays.asList(12, 56, 17, 21, 94, 34);
+     
+	static void commonElements() {
+		list1.stream().filter(list2::contains).forEach(System.out::println);
+	}
+
+	public static void main(String[] args) {
+		commonElements();
+	}
+
+}
