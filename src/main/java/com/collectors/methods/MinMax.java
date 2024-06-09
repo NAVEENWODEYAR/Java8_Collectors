53dce5a3a94e5d3a01a043a68416bc3612171c41
Naveen <naveenkrishnawodeyar@gmail.com>
2024-06-09 21:32:45 +0530

    Comparator.comparing(String::length)

--- src/main/java/com/collectors/methods/MinMax.java
+++ src/main/java/com/collectors/methods/MinMax.java
@@ -10,8 +10,10 @@ public class MinMax {
 
 	static void maxLength() {
         List<String> stationeryList = Arrays.asList("Pen", "Eraser", "Note Book", "Pen", "Pencil", "Stapler", "Note Book", "Pencil");
-        Optional<String> max = stationeryList.stream().max(Comparator.comparing(String::length));
-        System.out.println(max.get());
+        String max = stationeryList.stream().max(Comparator.comparing(String::length)).get();
+        Object[] array = stationeryList.parallelStream().sorted(Comparator.comparing(String::length)).distinct().toArray();
+        System.out.println(Arrays.toString(array));
+        System.out.println(max);
 	}
 	public static void main(String[] str) {
 		maxLength();
