f92eb888cc99d99a49b5d8644932c230f6efe363
Naveen Kumar K <108418552+NAVEENWODEYAR@users.noreply.github.com>
2024-10-19 19:19:49 +0530

    Logger added

--- src/main/java/com/collectors/arrList/MostRepeatedElement.java
+++ src/main/java/com/collectors/arrList/MostRepeatedElement.java
@@ -6,10 +6,12 @@ import java.util.function.Function;
 import java.util.stream.Collectors;
 /**
  * @author Naveen K Wodeyar
- * @apiNote,find the most repeated element in an array
+ * @apiNote find the most repeated element in an array
  *
  */
 public class MostRepeatedElement {
+
+private static final Logger log = LoggerFactory.getLogger(MostRepeatedElement.class);
 
     static List<String> listOfStrings = Arrays.asList("Pen", "Eraser", "Note Book", "Pen", "Pencil", "Pen", "Note Book", "Pencil");
 
