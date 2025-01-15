8fba67a9eb4198450ddcda39a7f7f95c8e986e31
Naveen Kumar K <108418552+NAVEENWODEYAR@users.noreply.github.com>
2025-01-15 21:50:41 +0530

    logger added

--- src/main/java/com/collectors/RealTimeQueriesJ8CollectorsApplication.java
+++ src/main/java/com/collectors/RealTimeQueriesJ8CollectorsApplication.java
@@ -5,12 +5,16 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.log4j.*;
 
 @SpringBootApplication
 @OpenAPIDefinition
 public class RealTimeQueriesJ8CollectorsApplication {
 
+ private static final Logger log = LoggerFactory.getLogger(RealTimeQueriesJ8CollectorsApplication.class);
+
 	public static void main(String[] args) {
 		SpringApplication.run(RealTimeQueriesJ8CollectorsApplication.class, args);
+log.info("Application Started");
 	}
 }
