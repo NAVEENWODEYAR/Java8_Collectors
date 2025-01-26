bcea1ce3157c577a017842541e0ab219731b19f9
Naveen <naveenkrishnawodeyar@gmail.com>
2025-01-26 20:45:12 +0530

    Anonymous array

--- src/main/java/com/collectors/arrList/ArrList.java
+++ src/main/java/com/collectors/arrList/ArrList.java
@@ -1,6 +1,7 @@
 package com.collectors.arrList;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 /**
@@ -10,15 +11,33 @@ import java.util.List;
 
 public class ArrList {
 
-	static void arrEx() {
-		System.out.println("/nArray/n");
-		String[] strArr = new String[10];
+	static void arrEx() { 
+		System.out.println("\nArray\n");
+		String[] strArr = new String[10];//declaration
+		try {
+			System.out.println("strArr "+strArr[strArr.length]);
+		} catch (Exception e) {
+			System.out.println("Exception "+e.getLocalizedMessage());
+		}
+		
+		int a[]={33,3,4,5};//declaration, instantiation and initialization  
+		System.out.println("Accessing the elements of array");
+		for (int i = 0; i < a.length; i++) {
+			System.out.println(a[i]);
+		}
 		
 	}
 	
+	static void anonymousArray(String[] strArr) {
+		System.out.println("\nAnonymous Array");
+		for (int i = 0; i < strArr.length; i++) {
+			System.out.println(strArr[i]);
+		}
+	}
+	
 	public static void main(String[] args) {
-		System.out.println("/n Re-sizable array with Default initial capacity 10 & Load Factor 0.75F/n");
-		
+		System.out.println("\n Re-sizable array with Default initial capacity 10 & Load Factor 0.75F\n");
+		arrEx();
 		List<String> arrList = new ArrayList<String>(20);
 		System.out.println(arrList.size());
 		arrList.add("1");
@@ -27,6 +46,8 @@ public class ArrList {
 		arrList.remove(arrList.size()-1);
 		arrList.add(arrList.size()-1, null);
 		System.out.println(arrList.toString());
+		System.out.println("\n####\n");
+		anonymousArray(new String[]{"One","Two","Three","Four"});
 	}
 
 }
