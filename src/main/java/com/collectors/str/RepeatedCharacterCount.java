d3c39e5664679f35efbdc295a37aa57093801b95
Naveen <naveenkrishnawodeyar@gmail.com>
2025-01-27 22:29:20 +0530

    charCount in string

---
+++ src/main/java/com/collectors/str/RepeatedCharacterCount.java
@@ -0,0 +1,26 @@
+package com.collectors.str;
+
+/**
+ * @author NaveenWodeyar
+ *
+ */
+import java.util.*;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+public class RepeatedCharacterCount {
+    public static void main(String[] args) {
+        String input = "programming";
+
+        // Using streams to count occurrences of characters
+        Map<Character, Long> charCountMap = input.chars()
+                .mapToObj(c -> (char) c) // Convert int to char
+                .collect(Collectors.groupingBy(Function.identity(), Collectors.counting())); // Group by character and count occurrences
+
+        // Print characters that have more than 1 occurrence
+        System.out.println("Repeated characters:");
+        charCountMap.entrySet().stream()
+                .filter(entry -> entry.getValue() > 1) // Filter for repeated characters
+                .forEach(entry -> System.out.println(entry.getKey() + ": " + entry.getValue()));
+    }
+}
