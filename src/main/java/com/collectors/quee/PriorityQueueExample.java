ae1f0d3139b147d3cba0863b156cd5e23fd39cf0
Naveen <naveenkrishnawodeyar@gmail.com>
2025-03-07 23:03:00 +0530

    PriorityQueueExample UT added

--- src/main/java/com/collectors/quee/PriorityQueueExample.java
+++ src/main/java/com/collectors/quee/PriorityQueueExample.java
@@ -1,5 +1,6 @@
 package com.collectors.quee;
 
+import java.time.LocalDateTime;
 import java.util.PriorityQueue;
 
 /**
@@ -9,47 +10,54 @@ import java.util.PriorityQueue;
 
 public class PriorityQueueExample {
 
-	 public static void main(String[] args){
-		 
-	        //Creating a PriorityQueue with default Comparator.
-	        PriorityQueue<Integer> pQueue = new PriorityQueue<Integer>();
-	 
-	        //Inserting elements into pQueue.
-	 
-	        pQueue.offer(21);
-	 
-	        pQueue.offer(17);
-	 
-	        pQueue.offer(37);
-	 
-	        pQueue.offer(41);
-	 
-	        pQueue.offer(9);
-	 
-	        pQueue.offer(67);
-	 
-	        pQueue.offer(31);
-	 
-	        //Removing the head elements
-	 
-	        System.out.println(pQueue.poll());     //Output : 9
-	 
-	        System.out.println(pQueue.poll());     //Output : 17
-	 
-	        System.out.println(pQueue.poll());     //Output : 21
-	 
-	        System.out.println(pQueue.poll());     //Output : 31
-	 
-	        System.out.println(pQueue.poll());     //Output : 37
-	 
-	        System.out.println(pQueue.poll());     //Output : 41
-	 
-	        System.out.println(pQueue.poll());     //Output : 67
-	        
-	        // Print and remove elements in priority order (descending)
-	        System.out.println("PriorityQueue (Descending Order): ");
-	        while (!pQueue.isEmpty()) {
-	            System.out.println(pQueue.poll());
-	        }
-	    }
+	public static void main(String[] args) {
+		System.out.println("PriorityQueueExample :{}"+LocalDateTime.now());
+	}
+
+	/**
+	 * @return
+	 */
+	public static PriorityQueue<Integer> createPriorityQueue() {
+		// Creating a PriorityQueue with default Comparator.
+		PriorityQueue<Integer> pQueue = new PriorityQueue<Integer>();
+
+		// Inserting elements into pQueue.
+
+		pQueue.offer(21);
+
+		pQueue.offer(17);
+
+		pQueue.offer(37);
+
+		pQueue.offer(41);
+
+		pQueue.offer(9);
+
+		pQueue.offer(67);
+
+		pQueue.offer(31);
+
+		// Removing the head elements
+
+		System.out.println(pQueue.poll()); // Output : 9
+
+		System.out.println(pQueue.poll()); // Output : 17
+
+		System.out.println(pQueue.poll()); // Output : 21
+
+		System.out.println(pQueue.poll()); // Output : 31
+
+		System.out.println(pQueue.poll()); // Output : 37
+
+		System.out.println(pQueue.poll()); // Output : 41
+
+		System.out.println(pQueue.poll()); // Output : 67
+
+		// Print and remove elements in priority order (descending)
+		System.out.println("PriorityQueue (Descending Order): ");
+		while (!pQueue.isEmpty()) {
+			System.out.println(pQueue.poll());
+		}
+		return pQueue;
+	}
 }
