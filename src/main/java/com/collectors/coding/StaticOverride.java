e7f468e962d838dfb9430a898fc73a1fe613b4de
Naveen <naveenkrishnawodeyar@gmail.com>
2025-04-17 00:05:21 +0530

    different approaches of for loop

---
+++ src/main/java/com/collectors/coding/StaticOverride.java
@@ -0,0 +1,25 @@
+package com.collectors.coding;
+
+/**
+ * @author NaveenWodeyar
+ *
+ */
+
+class Tata {
+    static void sayHi() {
+        System.out.println("Hi from Parent");
+    }
+}
+
+class Appa extends Tata {
+    static void sayHi() {
+        System.out.println("Hi from Child");
+    }
+}
+
+public class StaticOverride {
+    public static void main(String[] args) {
+        Tata t = new Appa();
+        t.sayHi(); // ?
+    }
+}
